71494a8533f9f835f3297e876af3ee409083e3a0
Chinonso Solomon <nnadisolomon1@gmail.com>
2025-11-01 12:53:40 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const interns = [
+{intern:"Mira",
+  scores: [10,8,9,8,8]
+},
+{intern:"Toby",
+  scores: [9,8,10,8,7]
+},
+{intern:"Esther",
+  scores: [7,8,9,10,7]
+},
+{intern:"Dera",
+  scores: [8,8,10,7,8]
+},
+{intern:"Batom",
+  scores: [10,9,9,7,8]
+}
+];
+
+//to calculate average scores
+function calculateAverage(scores){
+ const total = scores.reduce((sum, score)=>sum + score,0);
+  return total/scores.length;
+}
+console.log("Grades for each interns");
+interns.forEach(upskillers=>{
+  const average = calculateAverage(upskillers.scores);
+console.log(`${upskillers.intern}- Average Score: ${average}`)
+})
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+console.log("Intern GradeBook Results:");
+// interns.forEach(intern=>{
+//   const average = calculateAverage(intern.scores);
+//   console.log(`${intern.intern} -Average Score:${average}`)
+// })
